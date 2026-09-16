e61c6d5b0713eadf2e2375df2cde60daf0a1b958
NBnY5A <victor.lito@a.ucb.br>
2026-09-16 11:15:59 -0300

    feat: Create SplashPage

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.brandTitle}>GamePlay</Text>
     </View>
   );
 }
@@ -13,8 +11,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0E1647",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  brandTitle: {
+    fontFamily: "Rajdhani_700Bold",
+    fontSize: 64,
+    color: "#fff",
   },
 });
